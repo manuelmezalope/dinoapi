f6fb45d98d5caed5f5e23b872ebd215dce9250e7
manuelmezalope <mezalopem@gmail.com>
2025-12-26 01:29:25 -0500

    Update MenuComp.tsx

--- src/components/MenuComp.tsx
+++ src/components/MenuComp.tsx
@@ -2,7 +2,6 @@
 //                                                                                         REACT
 //=====================================================================================================================================================================================================
 import { Link } from "react-router";
-import { useState } from "react";
 import { useTheme } from '../context/ThemeContext';
 
 //=====================================================================================================================================================================================================
@@ -13,40 +12,46 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import react from '../assets/react.svg';
 import { Button } from "react-bootstrap";
-import Collapse from 'react-bootstrap/Collapse';
 
 export default function MenuComp() {
     const { botonBg, textBotonBg, botonBgBody, textBotonBgBody, setBotonBg, setTextBotonBg, setBotonBgBody, setTextBotonBgBody } = useTheme();
 
-    const [open, setOpen] = useState(false);
+    function cambiarIcono() {
+        const icono = document.querySelector('i') as HTMLElement;
+
+        if (icono?.classList.contains('bi-brightness-high')) {
+            icono.classList.remove("bi-brightness-high");
+            icono.classList.add("bi-moon");
+
+            setBotonBg('light');
+            setTextBotonBg('text-dark');
+            setBotonBgBody('dark');
+            setTextBotonBgBody('text-light');
+        } else {
+            icono.classList.remove("bi-moon");
+            icono.classList.add("bi-brightness-high");
+
+            setBotonBg('dark');
+            setTextBotonBg('text-light');
+            setBotonBgBody('light');
+            setTextBotonBgBody('text-dark');
+        }
+    }
 
     return (<header>
-        <span className={`${textBotonBgBody}`}></span><span className={`${botonBgBody}`}></span>{/*Esto no hace nada*/}
-        <Navbar bg={botonBg} data-bs-theme={botonBg} className="d-flex justify-content-center" style={{transition: "1s"}}>
-            <Container className={`${textBotonBg}`}>
-                <Navbar.Brand><img src={react}></img>React</Navbar.Brand>
+        <span className={`${textBotonBgBody}`}></span><span className={`${botonBgBody}`}></span><span className={`${textBotonBg}`}></span>{/*Esto no hace nada*/}
+        <Navbar bg="dark" data-bs-theme={botonBg} className="d-flex justify-content-center" style={{ transition: "1s", borderBottom: "2px white solid" }}>
+            <Container>
+                <Navbar.Brand><img src={react}></img></Navbar.Brand>
 
                 <Nav className="d-flex justify-content-center">
                     <div className="p-1">
-                        <Link className={`text-decoration-none ${textBotonBg}`} to={''}>Inicio</Link>&nbsp;&nbsp;
-                        <Link className={`text-decoration-none ${textBotonBg}`} to={'dinosaurios'}>Dinosaurios</Link>&nbsp;
+                        <Link className={`text-decoration-none text-light`} to={''}>Inicio</Link>&nbsp;&nbsp;
+                        <Link className={`text-decoration-none text-light`} to={'dinosaurios'}>Dinosaurios</Link>&nbsp;
                     </div>
 
                     <div>
-                        <Button className={`border border-${botonBgBody}`} onClick={() => { setOpen(!open) }} aria-controls="example-collapse-text" aria-expanded={open} style={{ transform: open ? 'rotate(180deg)' : 'rotate(0deg)', transition: "1s", background: "transparent"}}>
-                            {
-                                botonBg==='dark'
-                                    ? <i className="bi bi-caret-up-fill"></i>
-                                    : <i className="bi bi-caret-up-fill" style={{color: "black"}}></i>
-                            }
-                        </Button>
-
-                        <Collapse in={open} className="position-absolute top-55 end-0 mt-2 p-2 bg-body rounded shadow">
-                            <div id="example-collapse-text">
-                                <Button variant="light" onClick={() => { setBotonBg('dark'); setTextBotonBg('text-light'); setBotonBgBody('light'); setTextBotonBgBody('text-dark') }}>Blanco</Button>
-                                <Button variant="dark" onClick={() => { setBotonBg('light'); setTextBotonBg('text-dark'); setBotonBgBody('dark'); setTextBotonBgBody('text-light') }}>Negro</Button>
-                            </div>
-                        </Collapse>
+                        <Button variant="dark" onClick={() => cambiarIcono()}><i className="bi bi-brightness-high"></i></Button>
                     </div>
                 </Nav>
             </Container>
